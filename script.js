5e9ba734e75f915ee9a61253cf24a276997e8db0
Tharan <tharan0026@gmail.com>
2024-07-31 10:25:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-function check(){
-    const pan=document.getElementById("num").value;
+function check() {
+    const pan = document.getElementById("num").value;
 
-    if(pan==="")
+    if (pan === "") {
         alert("Enter your PAN number");
-    else if(pan.length!==10)
+    } else if (pan.length !== 10) {
         alert("Invalid PAN number");
-    else if(pan==="/^[A-Z]{5}[0-9]{4}[A-Z]{1}")
-        alert("Invalid PAN number");
-    else{
-        document.getElementById("result").innerHTML="Valid PAN Number";
-        pan.value="";
+    } else {
+        const panRegex = /^[A-Z]{5}[0-9]{4}[A-Z]{1}$/;
+        if (!panRegex.test(pan)) {
+            document.getElementById("result").innerHTML = "Invalid PAN Number";
+        } else {
+            document.getElementById("result").innerHTML = "Valid PAN Number";
+        }
     }
 }
